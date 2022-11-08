70ce4d68fc5b2d7f067a6d7b8041e2f1ddf9943c
lalyos <lalyos@yahoo.com>
2022-11-08 15:55:56 +0100

    introducing TITLE/COLOR/BODY env vars

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo ${TITLE:=Hello} > /var/www/html/index.html
+cat > /var/www/html/index.html <<EOF
+<html>
+<body bgcolor="${COLOR:=gray}">
+<h1>${TITLE:=Hello}</h1>
+
+${BODY:=Please use TITLE/COLOR/BODY env vars}
+</body>
+</html>
+EOF
 
 nginx -g "daemon off;"
